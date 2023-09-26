10ac7b4a0f7e8e734ddcdbe4ade33a553927f986
juan-gon <jgonzales@peruyoung.com>
2023-09-26 19:30:18 +0200

    delete contect

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -3,11 +3,12 @@ import { EntityNotFoundError, Equal, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { CreateContactDto } from './dto/create-contact.dto';
-import { UpdateContactDto } from './dto/update-contact.dto';
+// import { UpdateContactDto } from './dto/update-contact.dto';
 import { ExceptionService } from '../services/exception.service';
 import { Contact } from './entities/contact.entity';
 import { User } from '../user/entities/user.entity';
 import { isUUID } from 'class-validator';
+import { DeleteContactDto } from './dto/delete-contact.dto';
 
 @Injectable()
 export class ContactService {
@@ -17,60 +18,6 @@ export class ContactService {
     @InjectRepository(Contact) private contactRepository: Repository<Contact>,
     private exceptionService: ExceptionService
   ) {}
-
-  // async create(createContactDto: CreateContactDto, user: User) {
-  //   try {
-
-  //   if (!isUUID(createContactDto.contactId))
-  //     throw new NotFoundException(`Contact with id ${createContactDto.contactId} not founds`);
-
-  //   let contact: User = await this.userRepository.findOneBy( { id: createContactDto.contactId } );
-
-  //   if (!contact)
-  //     throw new NotFoundException(`Contact with id ${createContactDto.contactId} not found`);
-      
-  //   if (createContactDto.contactId == user.id)
-  //     throw new NotFoundException(`You cannot add yourself`);
-    
-
-  //     const existingChatUser = await this.contactRepository.findOne({
-  //       where: {
-  //         user: { id: user.id },
-  //         contact: { id: contact.id },
-  //       },
-  //     });
-  //     if (existingChatUser)
-  //       throw new NotFoundException(`Contact whith id ${createContactDto.contactId} exist no add`);
-
-  //     const newContact = this.contactRepository.create({
-  //       user: user,
-  //       contact: contact,
-  //     });
-
-  //     const newContact2 = this.contactRepository.create({
-  //       user: contact,
-  //       contact: user,
-  //     });
-        
-  //     await this.contactRepository.save(newContact);
-  //     await this.contactRepository.save(newContact2);
-
-  //     const { password,email,isActive,roles, ...resContact } = contact;
-  //     return {
-  //       message: "new contact",
-  //       status: true,
-  //       contact: resContact
-  //     };
-  //   } catch (error) {
-  //     // si el error es de tipo EntityNotFoundError, lanzar un error 404
-	// 		if (error instanceof EntityNotFoundError) {
-	// 			this.exceptionService.handleNotFoundException('Contact not found', `Contact with id not found.`);
-	// 		} else {
-	// 			// si no, lanzar un error
-	// 			this.exceptionService.handleDBExceptions(error);
-	// 		}
-  //   }
-  // }
   
   async create(createContactDto: CreateContactDto, user: User) {
     try {
@@ -83,48 +30,43 @@ export class ContactService {
       // Verificar si ya existen los contactos
       const existingContact = await this.contactRepository.findOne({
           where: [
-              { user: { id: user.id }, contact: { id: contactId } },
-              { user: { id: contactId }, contact: { id: user.id } }
-          ]
-      });
-      
-      if (existingContact) {
-          throw new ConflictException(`Contact with id ${contactId} already exists`);
+                { user: { id: user.id }, contact: { id: contactId } },
+                { user: { id: contactId }, contact: { id: user.id } }
+            ]
+        });
+        
+        if (existingContact) {
+            throw new ConflictException(`Contact with id ${contactId} already exists`);
+        }
+
+          // Verificar la existencia del contacto
+        const contactData = await this.userRepository.findOne({
+          where: { id: contactId }
+        });
+
+        if (!contactData) {
+            throw new NotFoundException(`Contact with id ${contactId} not found`);
+        }
+
+        const newContact = this.contactRepository.create({ user, contact: contactData });
+        const reciprocalContact = this.contactRepository.create({ user: contactData, contact: user });
+
+        await this.contactRepository.save([newContact, reciprocalContact]);
+
+        const { password, email, isActive, roles, ...resContact } = contactData;
+        return {
+            message: "new contact",
+            status: true,
+            contact: resContact
+        };
+      } catch (error) {
+        if (error instanceof EntityNotFoundError) {
+            this.exceptionService.handleNotFoundException('Contact not found', `Contact with id not found.`);
+        } else {
+            this.exceptionService.handleDBExceptions(error);
+        }
       }
-
-        // Verificar la existencia del contacto
-      const contactData = await this.userRepository.findOne({
-        where: { id: contactId }
-      });
-
-      if (!contactData) {
-          throw new NotFoundException(`Contact with id ${contactId} not found`);
-      }
-
-      const newContact = this.contactRepository.create({ user, contact: contactData });
-      const reciprocalContact = this.contactRepository.create({ user: contactData, contact: user });
-
-      await this.contactRepository.save([newContact, reciprocalContact]);
-
-      const { password, email, isActive, roles, ...resContact } = contactData;
-      return {
-          message: "new contact",
-          status: true,
-          contact: resContact
-      };
-    } catch (error) {
-      if (error instanceof EntityNotFoundError) {
-          this.exceptionService.handleNotFoundException('Contact not found', `Contact with id not found.`);
-      } else {
-          this.exceptionService.handleDBExceptions(error);
-      }
-    }
-}
-
-
-  // findAll() {
-  //   return `This action returns all contact`;
-  // }
+  }
 
   async findOne(id: string) {
     try {
@@ -163,7 +105,44 @@ export class ContactService {
   //   return `This action updates a #${id} contact`;
   // }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} contact`;
-  // }
+  async remove(deleteContactDto: DeleteContactDto, user: User) {
+    try {
+        const { contactId } = deleteContactDto;
+        if (contactId === user.id) {
+            throw new BadRequestException('You cannot delete yourself');
+        }
+        const existingContact = await this.contactRepository.findOne({
+            where: [
+                { user: { id: user.id }, contact: { id: contactId } },
+                { user: { id: contactId }, contact: { id: user.id } }
+            ]
+        });
+        console.log(existingContact.id);
+
+        if (!existingContact) {
+            throw new NotFoundException(`Contact with id ${contactId} not found`);
+        }
+
+        const deleteQueryBuilder = this.contactRepository.createQueryBuilder();
+
+        await deleteQueryBuilder
+            .delete()
+            .from(Contact)
+            .where("userId = :userId AND contactId = :contactId", { userId: user.id, contactId: contactId })
+            .orWhere("userId = :contactId AND contactId = :userId", { userId: user.id, contactId: contactId })
+            .execute();
+
+        return {
+            message: "Contact deleted successfully",
+            status: true
+        };
+
+    } catch (error) {
+        if (error instanceof EntityNotFoundError) {
+            this.exceptionService.handleNotFoundException('Contact not found', `Contact with id not found.`);
+        } else {
+            this.exceptionService.handleDBExceptions(error);
+        }
+    }
+  }
 }
